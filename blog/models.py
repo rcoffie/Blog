0481583eb518cbf00aa152cf6330415b3a45b8e3
Robert Coffie <rcoffie22@yahoo.com>
2021-02-07 13:47:42 +0000

    tailwind to bootstrap

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   body   = models.TextField()
   postImage = models.ImageField(upload_to='photos/%Y/%m/%d')
+  likes = models.ManyToManyField(User,related_name='blog_post', blank=True)
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
   status   = models.CharField(max_length=15, choices=status_choices, default='draft', null=True)
